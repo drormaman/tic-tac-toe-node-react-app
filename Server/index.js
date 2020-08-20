a84932d521051eedabac987e497f65a3a43b5a4b
drormaman <drormaman@gmail.com>
2020-08-20 15:53:35 +0300

    serve build using server

--- Server/index.js
+++ Server/index.js
@@ -1,43 +1,41 @@
-  
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 
+app.use(express.static(path.join(__dirname,"..","Client","tic-tac-toe","build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"..","Client","tic-tac-toe","build","index.html"));
+});
 
 app.get("/api/v1/records", (req, res) => {
-    fs.readFile('./records.json', (err, data) => {
-        if(err){
+    fs.readFile("./records.json", (err, data) => {
+        if (err) {
             throw err;
         }
-        // console.log(data);
         console.log(JSON.parse(data));
         res.send(JSON.parse(data));
     });
-    // res.send(records);
 });
 
 app.post("/api/v1/records", (req, res) => {
-
-    fs.readFile('./records.json', (err, data) => {
-        if(err){
+    fs.readFile("./records.json", (err, data) => {
+        if (err) {
             throw err;
         }
         const dataArr = JSON.parse(data);
         dataArr.push(req.body);
-        console.log(dataArr)
-        fs.writeFile('./records.json', JSON.stringify(dataArr), (err) => {
-            if(err){
+        console.log(dataArr);
+        fs.writeFile("./records.json", JSON.stringify(dataArr), (err) => {
+            if (err) {
                 throw err;
             }
-            res.send('OK')
-        })
+            res.send("user was added successfully");
+        });
     });
-
-    
 });
 
 app.listen(8080);
